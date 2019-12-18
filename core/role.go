e063fbc4c8954549e7ab24bce0431479ab4422e7
zunaira1231 <zunaira.siddique@cloudplex.io>
2019-12-18 17:43:28 +0500

    role , role binding, service account & kubernetes service server side implementation added

--- core/role.go
+++ core/role.go
@@ -3,310 +3,214 @@ package core
 import (
 	"context"
 	"encoding/json"
-	"fmt"
+
 	"google.golang.org/grpc"
 	"istio-service-mesh/constants"
 	pb "istio-service-mesh/core/proto"
 	"istio-service-mesh/utils"
-	"istio.io/api/networking/v1alpha3"
-	istioClient "istio.io/client-go/pkg/apis/networking/v1alpha3"
-	"strings"
 )
 
-type RoleServer struct{
+type RoleServer struct {
 }
-func (s *RoleServer)CreateRole(ctx context.Context,req *pb.RoleService)(*pb.ServiceResponse,error){
+
+func (s *RoleServer) CreateRole(ctx context.Context, req *pb.RoleService) (*pb.ServiceResponse, error) {
 	serviceResp := new(pb.ServiceResponse)
 	serviceResp.Status = &pb.ServiceStatus{
-		Id: req.ServiceId,
+		Id:        req.ServiceId,
 		ServiceId: req.ServiceId,
-		Name: req.Name,
+		Name:      req.Name,
 	}
-	ksdRequest ,err := getRoleRequestObject(req)
-
+	ksdRequest := req
+	conn, err := grpc.DialContext(ctx, constants.K8sEngineGRPCURL, grpc.WithInsecure())
 	if err != nil {
 		utils.Error.Println(err)
-		getErrorResp(serviceResp,err)
-		return serviceResp,err
-	}
-
-	conn, err := grpc.DialContext(ctx,constants.K8sEngineGRPCURL,grpc.WithInsecure())
-	if err != nil {
-		utils.Error.Println(err)
-		getErrorResp(serviceResp,err)
-		return serviceResp,err
-	}
-	defer conn.Close()
-
-
-	raw, err := json.Marshal(ksdRequest)
-	if err != nil {
-		utils.Error.Println(err)
-		getErrorResp(serviceResp,err)
-		return serviceResp,err
-	}
-	result, err := pb.NewServiceClient(conn).CreateService(ctx,&pb.ServiceRequest{
-		ProjectId:req.ProjectId,
-		Service: raw,
-		CompanyId:req.CompanyId,
-		Token: req.Token,
-	})
-	if err != nil {
-		utils.Error.Println(err)
-		getErrorResp(serviceResp,err)
-		return serviceResp,err
-	}
-	utils.Info.Println(result.Service)
-	serviceResp.Status.Status = "successful"
-	serviceResp.Status.StatusIndividual = append(serviceResp.Status.StatusIndividual ,"successful")
-
-	return serviceResp,nil
-
-}
-func (s *RoleServer)GetRole(ctx context.Context,req *pb.RoleService)(*pb.ServiceResponse,error){
-	serviceResp := new(pb.ServiceResponse)
-	serviceResp.Status = &pb.ServiceStatus{
-		Id: req.ServiceId,
-		ServiceId: req.ServiceId,
-		Name: req.Name,
-	}
-	ksdRequest ,err := getRoleRequestObject(req)
-
-	if err != nil {
-		utils.Error.Println(err)
-		getErrorResp(serviceResp,err)
-		return serviceResp,err
-	}
-
-	conn, err := grpc.DialContext(ctx,constants.K8sEngineGRPCURL,grpc.WithInsecure())
-	if err != nil {
-		utils.Error.Println(err)
-		getErrorResp(serviceResp,err)
-		return serviceResp,err
+		getErrorResp(serviceResp, err)
+		return serviceResp, err
 	}
 	defer conn.Close()
 
 	raw, err := json.Marshal(ksdRequest)
 	if err != nil {
 		utils.Error.Println(err)
-		getErrorResp(serviceResp,err)
-		return serviceResp,err
+		getErrorResp(serviceResp, err)
+		return serviceResp, err
 	}
-	result, err := pb.NewServiceClient(conn).GetService(ctx,&pb.ServiceRequest{
-		ProjectId:req.ProjectId,
-		Service: raw,
-		CompanyId:req.CompanyId,
-		Token: req.Token,
+	result, err := pb.NewServiceClient(conn).CreateService(ctx, &pb.ServiceRequest{
+		ProjectId: req.ProjectId,
+		Service:   raw,
+		CompanyId: req.CompanyId,
+		Token:     req.Token,
 	})
 	if err != nil {
 		utils.Error.Println(err)
-		getErrorResp(serviceResp,err)
-		return serviceResp,err
+		getErrorResp(serviceResp, err)
+		return serviceResp, err
 	}
 	utils.Info.Println(result.Service)
 	serviceResp.Status.Status = "successful"
-	serviceResp.Status.StatusIndividual = append(serviceResp.Status.StatusIndividual ,"successful")
+	serviceResp.Status.StatusIndividual = append(serviceResp.Status.StatusIndividual, "successful")
 
-	return serviceResp,nil
+	return serviceResp, nil
+
 }
-func (s *RoleServer)DeleteRole(ctx context.Context,req *pb.RoleService)(*pb.ServiceResponse,error){
+func (s *RoleServer) GetRole(ctx context.Context, req *pb.RoleService) (*pb.ServiceResponse, error) {
 	serviceResp := new(pb.ServiceResponse)
 	serviceResp.Status = &pb.ServiceStatus{
-		Id: req.ServiceId,
+		Id:        req.ServiceId,
 		ServiceId: req.ServiceId,
-		Name: req.Name,
+		Name:      req.Name,
 	}
-	ksdRequest ,err := getRoleRequestObject(req)
+	ksdRequest := req
 
+	conn, err := grpc.DialContext(ctx, constants.K8sEngineGRPCURL, grpc.WithInsecure())
 	if err != nil {
 		utils.Error.Println(err)
-		getErrorResp(serviceResp,err)
-		return serviceResp,err
-	}
-
-	conn, err := grpc.DialContext(ctx,constants.K8sEngineGRPCURL,grpc.WithInsecure())
-	if err != nil {
-		utils.Error.Println(err)
-		getErrorResp(serviceResp,err)
-		return serviceResp,err
+		getErrorResp(serviceResp, err)
+		return serviceResp, err
 	}
 	defer conn.Close()
 
 	raw, err := json.Marshal(ksdRequest)
 	if err != nil {
 		utils.Error.Println(err)
-		getErrorResp(serviceResp,err)
-		return serviceResp,err
+		getErrorResp(serviceResp, err)
+		return serviceResp, err
 	}
-	result, err := pb.NewServiceClient(conn).DeleteService(ctx,&pb.ServiceRequest{
-		ProjectId:req.ProjectId,
-		Service: raw,
-		CompanyId:req.CompanyId,
-		Token: req.Token,
+	result, err := pb.NewServiceClient(conn).GetService(ctx, &pb.ServiceRequest{
+		ProjectId: req.ProjectId,
+		Service:   raw,
+		CompanyId: req.CompanyId,
+		Token:     req.Token,
 	})
 	if err != nil {
 		utils.Error.Println(err)
-		getErrorResp(serviceResp,err)
-		return serviceResp,err
+		getErrorResp(serviceResp, err)
+		return serviceResp, err
 	}
 	utils.Info.Println(result.Service)
 	serviceResp.Status.Status = "successful"
-	serviceResp.Status.StatusIndividual = append(serviceResp.Status.StatusIndividual ,"successful")
+	serviceResp.Status.StatusIndividual = append(serviceResp.Status.StatusIndividual, "successful")
 
-	return serviceResp,nil
+	return serviceResp, nil
 }
-func (s *RoleServer)PatchRole(ctx context.Context,req *pb.RoleService)(*pb.ServiceResponse,error){
+func (s *RoleServer) DeleteRole(ctx context.Context, req *pb.RoleService) (*pb.ServiceResponse, error) {
 	serviceResp := new(pb.ServiceResponse)
 	serviceResp.Status = &pb.ServiceStatus{
-		Id: req.ServiceId,
+		Id:        req.ServiceId,
 		ServiceId: req.ServiceId,
-		Name: req.Name,
+		Name:      req.Name,
 	}
-	ksdRequest ,err := getRoleRequestObject(req)
+	ksdRequest := req
 
+	conn, err := grpc.DialContext(ctx, constants.K8sEngineGRPCURL, grpc.WithInsecure())
 	if err != nil {
 		utils.Error.Println(err)
-		getErrorResp(serviceResp,err)
-		return serviceResp,err
-	}
-
-	conn, err := grpc.DialContext(ctx,constants.K8sEngineGRPCURL,grpc.WithInsecure())
-	if err != nil {
-		utils.Error.Println(err)
-		getErrorResp(serviceResp,err)
-		return serviceResp,err
+		getErrorResp(serviceResp, err)
+		return serviceResp, err
 	}
 	defer conn.Close()
 
 	raw, err := json.Marshal(ksdRequest)
 	if err != nil {
 		utils.Error.Println(err)
-		getErrorResp(serviceResp,err)
-		return serviceResp,err
+		getErrorResp(serviceResp, err)
+		return serviceResp, err
 	}
-	result, err := pb.NewServiceClient(conn).PatchService(ctx,&pb.ServiceRequest{
-		ProjectId:req.ProjectId,
-		Service: raw,
-		CompanyId:req.CompanyId,
-		Token: req.Token,
+	result, err := pb.NewServiceClient(conn).DeleteService(ctx, &pb.ServiceRequest{
+		ProjectId: req.ProjectId,
+		Service:   raw,
+		CompanyId: req.CompanyId,
+		Token:     req.Token,
 	})
 	if err != nil {
 		utils.Error.Println(err)
-		getErrorResp(serviceResp,err)
-		return serviceResp,err
+		getErrorResp(serviceResp, err)
+		return serviceResp, err
 	}
 	utils.Info.Println(result.Service)
 	serviceResp.Status.Status = "successful"
-	serviceResp.Status.StatusIndividual = append(serviceResp.Status.StatusIndividual ,"successful")
+	serviceResp.Status.StatusIndividual = append(serviceResp.Status.StatusIndividual, "successful")
 
-	return serviceResp,nil
+	return serviceResp, nil
 }
-func (s *RoleServer)PutRole(ctx context.Context,req *pb.RoleService)(*pb.ServiceResponse,error){
+func (s *RoleServer) PatchRole(ctx context.Context, req *pb.RoleService) (*pb.ServiceResponse, error) {
 	serviceResp := new(pb.ServiceResponse)
 	serviceResp.Status = &pb.ServiceStatus{
-		Id: req.ServiceId,
+		Id:        req.ServiceId,
 		ServiceId: req.ServiceId,
-		Name: req.Name,
+		Name:      req.Name,
 	}
-	ksdRequest ,err := getRoleRequestObject(req)
+	ksdRequest := req
 
+	conn, err := grpc.DialContext(ctx, constants.K8sEngineGRPCURL, grpc.WithInsecure())
 	if err != nil {
 		utils.Error.Println(err)
-		getErrorResp(serviceResp,err)
-		return serviceResp,err
-	}
-
-	conn, err := grpc.DialContext(ctx,constants.K8sEngineGRPCURL,grpc.WithInsecure())
-	if err != nil {
-		utils.Error.Println(err)
-		getErrorResp(serviceResp,err)
-		return serviceResp,err
+		getErrorResp(serviceResp, err)
+		return serviceResp, err
 	}
 	defer conn.Close()
 
 	raw, err := json.Marshal(ksdRequest)
 	if err != nil {
 		utils.Error.Println(err)
-		getErrorResp(serviceResp,err)
-		return serviceResp,err
+		getErrorResp(serviceResp, err)
+		return serviceResp, err
 	}
-	result, err := pb.NewServiceClient(conn).PutService(ctx,&pb.ServiceRequest{
-		ProjectId:req.ProjectId,
-		Service: raw,
-		CompanyId:req.CompanyId,
-		Token: req.Token,
+	result, err := pb.NewServiceClient(conn).PatchService(ctx, &pb.ServiceRequest{
+		ProjectId: req.ProjectId,
+		Service:   raw,
+		CompanyId: req.CompanyId,
+		Token:     req.Token,
 	})
 	if err != nil {
 		utils.Error.Println(err)
-		getErrorResp(serviceResp,err)
-		return serviceResp,err
+		getErrorResp(serviceResp, err)
+		return serviceResp, err
 	}
 	utils.Info.Println(result.Service)
 	serviceResp.Status.Status = "successful"
-	serviceResp.Status.StatusIndividual = append(serviceResp.Status.StatusIndividual ,"successful")
+	serviceResp.Status.StatusIndividual = append(serviceResp.Status.StatusIndividual, "successful")
 
-	return serviceResp,nil
+	return serviceResp, nil
 }
-
-
-func getRole(input *pb.RoleService) (*istioClient.Gateway, error) {
-	var istioServ = new(istioClient.Gateway)
-	labels := make(map[string]string)
-	labels["app"] = strings.ToLower(input.Name)
-	labels["name"] = strings.ToLower(input.Name)
-	labels["version"] = strings.ToLower(input.Version)
-	labels["namespace"] = strings.ToLower(input.Namespace)
-	istioServ.Labels = labels
-	istioServ.Kind = "Gateway"
-	istioServ.APIVersion = "networking.istio.io/v1alpha3"
-
-	gateway := v1alpha3.Gateway{}
-
-	gateway.Selector = input.ServiceAttributes.Selectors
-
-	raw, err := json.Marshal(input.ServiceAttributes)
-	if err != nil {
-		return nil, fmt.Errorf("unable to marshal gateway input object. Error: %s",err.Error())
-	}
-	err = json.Unmarshal(raw,gateway)
-	if err != nil {
-		return nil, fmt.Errorf("unable to unmarshal gateway input object to istio object. Error: %s",err.Error())
+func (s *RoleServer) PutRole(ctx context.Context, req *pb.RoleService) (*pb.ServiceResponse, error) {
+	serviceResp := new(pb.ServiceResponse)
+	serviceResp.Status = &pb.ServiceStatus{
+		Id:        req.ServiceId,
+		ServiceId: req.ServiceId,
+		Name:      req.Name,
 	}
 
-	istioServ.Spec = gateway
-	return istioServ, nil
-}
-func getRoleSpec() (v1alpha3.Gateway, error) {
+	ksdRequest := req
 
-	gateway := v1alpha3.Gateway{}
-	var hosts []string
-	hosts = append(hosts, "*")
-	var servers []*v1alpha3.Server
-
-	var serv v1alpha3.Server
-	serv.Port = &v1alpha3.Port{Name: strings.ToLower("HTTP"), Protocol: "HTTP", Number: uint32(80)}
-	serv.Hosts = hosts
-	servers = append(servers, &serv)
-
-	/*var serv2 v1alpha3.Server
-	serv2.Port = &v1alpha3.Port{Name: strings.ToLower("HTTPS"), Protocol: "HTTPS", Number: uint32(443)}
-	serv2.Hosts = hosts
-	servers = append(servers, &serv2)*/
-
-	selector := make(map[string]string)
-
-	selector["istio"] = "ingressgateway"
-	gateway.Selector = selector
-	gateway.Servers = servers
-	return gateway, nil
-}
-
-func getRoleRequestObject(req *pb.RoleService)(*istioClient.Gateway, error){
-	gtwReq, err := getIstioGateway(req)
+	conn, err := grpc.DialContext(ctx, constants.K8sEngineGRPCURL, grpc.WithInsecure())
 	if err != nil {
 		utils.Error.Println(err)
-
-		return nil,err
+		getErrorResp(serviceResp, err)
+		return serviceResp, err
 	}
-	return gtwReq,nil
+	defer conn.Close()
+
+	raw, err := json.Marshal(ksdRequest)
+	if err != nil {
+		utils.Error.Println(err)
+		getErrorResp(serviceResp, err)
+		return serviceResp, err
+	}
+	result, err := pb.NewServiceClient(conn).PutService(ctx, &pb.ServiceRequest{
+		ProjectId: req.ProjectId,
+		Service:   raw,
+		CompanyId: req.CompanyId,
+		Token:     req.Token,
+	})
+	if err != nil {
+		utils.Error.Println(err)
+		getErrorResp(serviceResp, err)
+		return serviceResp, err
+	}
+	utils.Info.Println(result.Service)
+	serviceResp.Status.Status = "successful"
+	serviceResp.Status.StatusIndividual = append(serviceResp.Status.StatusIndividual, "successful")
+
+	return serviceResp, nil
 }
