afc02dbba1fca44062905eb99e13213c087ecf2e
mahmad752 <m.ahmad@cloudplex.io>
2020-04-09 16:06:24 +0500

    vendor updated

---
+++ vendor/bitbucket.org/cloudplex-devs/microservices-mesh-engine/constants/services.go
@@ -0,0 +1,34 @@
+package constants
+
+const (
+	//------------------Services------------------------------------//
+
+	//---------k8s service type -----------------/////
+	DeploymentServiceType         = "deployment"
+	StatefulSetServiceType        = "statefulset"
+	DaemonSetServiceType          = "daemonset"
+	JobServiceType                = "job"
+	CronJobServiceType            = "cronjob"
+	KubernetesServiceType         = "kubernetes_service"
+	RoleServiceType               = "role"
+	RoleBindingServiceType        = "role_binding"
+	ClusterRoleServiceType        = "cluster_role"
+	ClusterRoleBindingServiceType = "cluster_role_binding"
+	ServiceAccountServiceType     = "service_account"
+	Resources                     = "resources"
+	SecretServiceType             = "secret"
+	ConfigMapServiceType          = "config_map"
+	PVCServiceType                = "persistent_volume_claim"
+	PVServiceType                 = "persistent_volume"
+	StorageClassServiceType       = "storage_class"
+	NetworkPolicyServiceType      = "network_policy"
+	HpaServiceType                = "hpa"
+
+	/////---------------Istio service type---------------//////
+
+	GatewayServiceType   = "gateway"
+	VirtualServiceType   = "virtual_service"
+	DestinationRulesType = "destination_rule"
+	ServiceEntryType     = "service_entry"
+	PolicyType           = "policy"
+)
