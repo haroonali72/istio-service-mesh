f9219b837c414b24da92712928929eb4390fb457
mahmad752 <m.ahmad@cloudplex.io>
2020-04-16 18:36:32 +0500

    fixed pv deployment tissue

--- core/persistentvoume.go
+++ core/persistentvoume.go
@@ -1,13 +1,9 @@
 package core
 
 import (
-	pb1 "bitbucket.org/cloudplex-devs/kubernetes-services-deployment/core/proto"
 	pb "bitbucket.org/cloudplex-devs/microservices-mesh-engine/core/services/proto"
 	"context"
-	"encoding/json"
 	"errors"
-	"google.golang.org/grpc"
-	"istio-service-mesh/constants"
 	"istio-service-mesh/utils"
 	core "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -20,7 +16,7 @@ func (s *Server) CreatePersistentVolume(ctx context.Context, req *pb.PersistentV
 		ServiceId: req.ServiceId,
 		Name:      req.Name,
 	}
-	ksdRequest, err := getPersistentVolume(req)
+	_, err := getPersistentVolume(req)
 
 	if err != nil {
 		utils.Error.Println(err)
@@ -28,32 +24,32 @@ func (s *Server) CreatePersistentVolume(ctx context.Context, req *pb.PersistentV
 		return serviceResp, err
 	}
 
-	conn, err := grpc.DialContext(ctx, constants.K8sEngineGRPCURL, grpc.WithInsecure())
-	if err != nil {
-		utils.Error.Println(err)
-		getErrorResp(serviceResp, err)
-		return serviceResp, err
-	}
-	defer conn.Close()
-
-	raw, err := json.Marshal(ksdRequest)
-	if err != nil {
-		utils.Error.Println(err)
-		getErrorResp(serviceResp, err)
-		return serviceResp, err
-	}
-	result, err := pb1.NewServiceClient(conn).CreateService(ctx, &pb1.ServiceRequest{
-		ProjectId: req.ProjectId,
-		Service:   raw,
-		CompanyId: req.CompanyId,
-		Token:     req.Token,
-	})
-	if err != nil {
-		utils.Error.Println(err)
-		getErrorResp(serviceResp, err)
-		return serviceResp, err
-	}
-	utils.Info.Println(result.Service)
+	//conn, err := grpc.DialContext(ctx, constants.K8sEngineGRPCURL, grpc.WithInsecure())
+	//if err != nil {
+	//	utils.Error.Println(err)
+	//	getErrorResp(serviceResp, err)
+	//	return serviceResp, err
+	//}
+	//defer conn.Close()
+	//
+	//raw, err := json.Marshal(ksdRequest)
+	//if err != nil {
+	//	utils.Error.Println(err)
+	//	getErrorResp(serviceResp, err)
+	//	return serviceResp, err
+	//}
+	//result, err := pb1.NewServiceClient(conn).CreateService(ctx, &pb1.ServiceRequest{
+	//	ProjectId: req.ProjectId,
+	//	Service:   raw,
+	//	CompanyId: req.CompanyId,
+	//	Token:     req.Token,
+	//})
+	//if err != nil {
+	//	utils.Error.Println(err)
+	//	getErrorResp(serviceResp, err)
+	//	return serviceResp, err
+	//}
+	//utils.Info.Println(result.Service)
 	serviceResp.Status.Status = "successful"
 	serviceResp.Status.StatusIndividual = append(serviceResp.Status.StatusIndividual, "successful")
 
@@ -79,7 +75,7 @@ func (s *Server) GetPersistentVolume(ctx context.Context, req *pb.PersistentVolu
 		ServiceId: req.ServiceId,
 		Name:      req.Name,
 	}
-	ksdRequest, err := getPersistentVolume(req)
+	_, err := getPersistentVolume(req)
 
 	if err != nil {
 		utils.Error.Println(err)
@@ -87,32 +83,32 @@ func (s *Server) GetPersistentVolume(ctx context.Context, req *pb.PersistentVolu
 		return serviceResp, err
 	}
 
-	conn, err := grpc.DialContext(ctx, constants.K8sEngineGRPCURL, grpc.WithInsecure())
-	if err != nil {
-		utils.Error.Println(err)
-		getErrorResp(serviceResp, err)
-		return serviceResp, err
-	}
-	defer conn.Close()
-
-	raw, err := json.Marshal(ksdRequest)
-	if err != nil {
-		utils.Error.Println(err)
-		getErrorResp(serviceResp, err)
-		return serviceResp, err
-	}
-	result, err := pb1.NewServiceClient(conn).GetService(ctx, &pb1.ServiceRequest{
-		ProjectId: req.ProjectId,
-		Service:   raw,
-		CompanyId: req.CompanyId,
-		Token:     req.Token,
-	})
-	if err != nil {
-		utils.Error.Println(err)
-		getErrorResp(serviceResp, err)
-		return serviceResp, err
-	}
-	utils.Info.Println(result.Service)
+	//conn, err := grpc.DialContext(ctx, constants.K8sEngineGRPCURL, grpc.WithInsecure())
+	//if err != nil {
+	//	utils.Error.Println(err)
+	//	getErrorResp(serviceResp, err)
+	//	return serviceResp, err
+	//}
+	//defer conn.Close()
+	//
+	//raw, err := json.Marshal(ksdRequest)
+	//if err != nil {
+	//	utils.Error.Println(err)
+	//	getErrorResp(serviceResp, err)
+	//	return serviceResp, err
+	//}
+	//result, err := pb1.NewServiceClient(conn).GetService(ctx, &pb1.ServiceRequest{
+	//	ProjectId: req.ProjectId,
+	//	Service:   raw,
+	//	CompanyId: req.CompanyId,
+	//	Token:     req.Token,
+	//})
+	//if err != nil {
+	//	utils.Error.Println(err)
+	//	getErrorResp(serviceResp, err)
+	//	return serviceResp, err
+	//}
+	//utils.Info.Println(result.Service)
 	serviceResp.Status.Status = "successful"
 	serviceResp.Status.StatusIndividual = append(serviceResp.Status.StatusIndividual, "successful")
 
@@ -125,7 +121,7 @@ func (s *Server) DeletePersistentVolume(ctx context.Context, req *pb.PersistentV
 		ServiceId: req.ServiceId,
 		Name:      req.Name,
 	}
-	ksdRequest, err := getPersistentVolume(req)
+	_, err := getPersistentVolume(req)
 
 	if err != nil {
 		utils.Error.Println(err)
@@ -133,32 +129,32 @@ func (s *Server) DeletePersistentVolume(ctx context.Context, req *pb.PersistentV
 		return serviceResp, err
 	}
 
-	conn, err := grpc.DialContext(ctx, constants.K8sEngineGRPCURL, grpc.WithInsecure())
-	if err != nil {
-		utils.Error.Println(err)
-		getErrorResp(serviceResp, err)
-		return serviceResp, err
-	}
-	defer conn.Close()
-
-	raw, err := json.Marshal(ksdRequest)
-	if err != nil {
-		utils.Error.Println(err)
-		getErrorResp(serviceResp, err)
-		return serviceResp, err
-	}
-	result, err := pb1.NewServiceClient(conn).DeleteService(ctx, &pb1.ServiceRequest{
-		ProjectId: req.ProjectId,
-		Service:   raw,
-		CompanyId: req.CompanyId,
-		Token:     req.Token,
-	})
-	if err != nil {
-		utils.Error.Println(err)
-		getErrorResp(serviceResp, err)
-		return serviceResp, err
-	}
-	utils.Info.Println(result.Service)
+	//conn, err := grpc.DialContext(ctx, constants.K8sEngineGRPCURL, grpc.WithInsecure())
+	//if err != nil {
+	//	utils.Error.Println(err)
+	//	getErrorResp(serviceResp, err)
+	//	return serviceResp, err
+	//}
+	//defer conn.Close()
+	//
+	//raw, err := json.Marshal(ksdRequest)
+	//if err != nil {
+	//	utils.Error.Println(err)
+	//	getErrorResp(serviceResp, err)
+	//	return serviceResp, err
+	//}
+	//result, err := pb1.NewServiceClient(conn).DeleteService(ctx, &pb1.ServiceRequest{
+	//	ProjectId: req.ProjectId,
+	//	Service:   raw,
+	//	CompanyId: req.CompanyId,
+	//	Token:     req.Token,
+	//})
+	//if err != nil {
+	//	utils.Error.Println(err)
+	//	getErrorResp(serviceResp, err)
+	//	return serviceResp, err
+	//}
+	//utils.Info.Println(result.Service)
 	serviceResp.Status.Status = "successful"
 	serviceResp.Status.StatusIndividual = append(serviceResp.Status.StatusIndividual, "successful")
 
@@ -171,7 +167,7 @@ func (s *Server) PatchPersistentVolume(ctx context.Context, req *pb.PersistentVo
 		ServiceId: req.ServiceId,
 		Name:      req.Name,
 	}
-	ksdRequest, err := getPersistentVolume(req)
+	_, err := getPersistentVolume(req)
 
 	if err != nil {
 		utils.Error.Println(err)
@@ -179,32 +175,32 @@ func (s *Server) PatchPersistentVolume(ctx context.Context, req *pb.PersistentVo
 		return serviceResp, err
 	}
 
-	conn, err := grpc.DialContext(ctx, constants.K8sEngineGRPCURL, grpc.WithInsecure())
-	if err != nil {
-		utils.Error.Println(err)
-		getErrorResp(serviceResp, err)
-		return serviceResp, err
-	}
-	defer conn.Close()
-
-	raw, err := json.Marshal(ksdRequest)
-	if err != nil {
-		utils.Error.Println(err)
-		getErrorResp(serviceResp, err)
-		return serviceResp, err
-	}
-	result, err := pb1.NewServiceClient(conn).PatchService(ctx, &pb1.ServiceRequest{
-		ProjectId: req.ProjectId,
-		Service:   raw,
-		CompanyId: req.CompanyId,
-		Token:     req.Token,
-	})
-	if err != nil {
-		utils.Error.Println(err)
-		getErrorResp(serviceResp, err)
-		return serviceResp, err
-	}
-	utils.Info.Println(result.Service)
+	//conn, err := grpc.DialContext(ctx, constants.K8sEngineGRPCURL, grpc.WithInsecure())
+	//if err != nil {
+	//	utils.Error.Println(err)
+	//	getErrorResp(serviceResp, err)
+	//	return serviceResp, err
+	//}
+	//defer conn.Close()
+	//
+	//raw, err := json.Marshal(ksdRequest)
+	//if err != nil {
+	//	utils.Error.Println(err)
+	//	getErrorResp(serviceResp, err)
+	//	return serviceResp, err
+	//}
+	//result, err := pb1.NewServiceClient(conn).PatchService(ctx, &pb1.ServiceRequest{
+	//	ProjectId: req.ProjectId,
+	//	Service:   raw,
+	//	CompanyId: req.CompanyId,
+	//	Token:     req.Token,
+	//})
+	//if err != nil {
+	//	utils.Error.Println(err)
+	//	getErrorResp(serviceResp, err)
+	//	return serviceResp, err
+	//}
+	//utils.Info.Println(result.Service)
 	serviceResp.Status.Status = "successful"
 	serviceResp.Status.StatusIndividual = append(serviceResp.Status.StatusIndividual, "successful")
 
@@ -217,7 +213,7 @@ func (s *Server) PutPersistentVolume(ctx context.Context, req *pb.PersistentVolu
 		ServiceId: req.ServiceId,
 		Name:      req.Name,
 	}
-	ksdRequest, err := getPersistentVolume(req)
+	_, err := getPersistentVolume(req)
 
 	if err != nil {
 		utils.Error.Println(err)
@@ -225,32 +221,32 @@ func (s *Server) PutPersistentVolume(ctx context.Context, req *pb.PersistentVolu
 		return serviceResp, err
 	}
 
-	conn, err := grpc.DialContext(ctx, constants.K8sEngineGRPCURL, grpc.WithInsecure())
-	if err != nil {
-		utils.Error.Println(err)
-		getErrorResp(serviceResp, err)
-		return serviceResp, err
-	}
-	defer conn.Close()
-
-	raw, err := json.Marshal(ksdRequest)
-	if err != nil {
-		utils.Error.Println(err)
-		getErrorResp(serviceResp, err)
-		return serviceResp, err
-	}
-	result, err := pb1.NewServiceClient(conn).PutService(ctx, &pb1.ServiceRequest{
-		ProjectId: req.ProjectId,
-		Service:   raw,
-		CompanyId: req.CompanyId,
-		Token:     req.Token,
-	})
-	if err != nil {
-		utils.Error.Println(err)
-		getErrorResp(serviceResp, err)
-		return serviceResp, err
-	}
-	utils.Info.Println(result.Service)
+	//conn, err := grpc.DialContext(ctx, constants.K8sEngineGRPCURL, grpc.WithInsecure())
+	//if err != nil {
+	//	utils.Error.Println(err)
+	//	getErrorResp(serviceResp, err)
+	//	return serviceResp, err
+	//}
+	//defer conn.Close()
+	//
+	//raw, err := json.Marshal(ksdRequest)
+	//if err != nil {
+	//	utils.Error.Println(err)
+	//	getErrorResp(serviceResp, err)
+	//	return serviceResp, err
+	//}
+	//result, err := pb1.NewServiceClient(conn).PutService(ctx, &pb1.ServiceRequest{
+	//	ProjectId: req.ProjectId,
+	//	Service:   raw,
+	//	CompanyId: req.CompanyId,
+	//	Token:     req.Token,
+	//})
+	//if err != nil {
+	//	utils.Error.Println(err)
+	//	getErrorResp(serviceResp, err)
+	//	return serviceResp, err
+	//}
+	//utils.Info.Println(result.Service)
 	serviceResp.Status.Status = "successful"
 	serviceResp.Status.StatusIndividual = append(serviceResp.Status.StatusIndividual, "successful")
 
